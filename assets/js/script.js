3887d98df77b4e2b9fcd0816b2b08fb427e4b124
23birola <23birola@gmail.com>
2024-01-19 20:05:09 +0000

    fixed code

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,8 +46,7 @@ async function getSpoonacularRandom() {
   }
 }
 
-getSpoonacularRandom()
-  .then((recipes => {
+getSpoonacularRandom().then((recipes => {
   recipes.forEach(recipe => {
   renderCard(recipe);
    })
@@ -56,15 +55,20 @@ getSpoonacularRandom()
 const recipeSection = $('#recipes');
 
 function renderCard(recipe) {
+  
   let cardEl = $('<div class="card" style="width: 18rem">');
+ // cardEl.css('background-image', 'url(' + recipe.image + ')');
   let cardImg = $('<img class="card-img-top" alt="recipe img">').attr('src', recipe.image);
   let cardInfoEl = $('<div class="card-body">');
   let cardTitle = $('<h5 class="card-title">').text(recipe.title);
+  let timeInfoEl = $('<div>');
+  let cookingTime = $('<p>').text(recipe.cookingMinutes);
   let timeIcon = $('<span class="material-symbols-outlined">timer </span>');
+  timeInfoEl.append(cookingTime, timeIcon);
   let caloriesIcon = $('<img width="25" height="25" src="https://img.icons8.com/external-ddara-lineal-ddara/64/external-calories-weight-loss-ddara-lineal-ddara.png" alt="external-calories-weight-loss-ddara-lineal-ddara"/>'); 
   let favoriteIcon = $('<i class="favorite material-icons" style="font-size: 35px; color: rgb(240, 127, 127)">').text('favorite');
 
-  cardInfoEl.append(timeIcon, caloriesIcon, favoriteIcon);
+  cardInfoEl.append(timeInfoEl, caloriesIcon, favoriteIcon);
   cardEl.append(cardImg, cardTitle, cardInfoEl);
   recipeSection.append(cardEl)
 }
